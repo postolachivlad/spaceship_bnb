4e93883677762fa716b6e2f772584da96537e390
Vladislav Postolachi <postolachi.vlad@gmail.com>
2020-01-04 17:09:22 +0100

    Drop user table and install devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :spaceships
   has_many :bookings
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   validates :email, presence: true,
                     uniqueness: true,
