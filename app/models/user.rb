9ebdc5ecb0b9c6819f6e72275781d84d7376dbb5
Vladislav Postolachi <postolachi.vlad@gmail.com>
2020-01-04 15:50:13 +0100

    Added validations for projects models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_many :spaceships
   has_many :bookings
+
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: /\A.*@.*\.com\z/ }
+  validates :password, presence: true,
+                       lenght: { minimum: 6 }
 end
